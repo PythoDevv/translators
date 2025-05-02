3edd4576d904ae8bd3ea08ab2559a151cd171e3e
Ilyos <karshiboevilypsbek@gmail.com>
2025-05-02 20:34:57 +0500

    fix:some

--- utils/db_api/postgresql.py
+++ utils/db_api/postgresql.py
@@ -75,9 +75,9 @@ class Database:
         sql = "INSERT INTO users (full_name, telegram_id, username, is_active) VALUES($1, $2, $3, $4) returning *"
         return await self.execute(sql, full_name, telegram_id, username, type, fetchrow=True)
 
-    async def add_json_file_user(self, full_name, username, telegram_id):
-        sql = "INSERT INTO users (full_name, username, telegram_id) VALUES($1, $2, $3) returning *"
-        return await self.execute(sql, full_name, username, telegram_id, fetchrow=True)
+    async def add_json_file_user(self, full_name, username, telegram_id,is_active=True):
+        sql = "INSERT INTO users (full_name, username, telegram_id,is_active) VALUES($1, $2, $3,$4) returning *"
+        return await self.execute(sql, full_name, username, telegram_id,is_active, fetchrow=True)
 
     async def select_all_users(self):
         sql = "SELECT * FROM Users"
